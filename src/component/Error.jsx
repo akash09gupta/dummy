af3ff9d629a59c12e4a8a15991e5aaf1bbe402bf
akash09gupta <akashgupta211ag@gmail.com>
2025-09-05 00:41:44 +0530

    done

--- src/component/Error.jsx
+++ src/component/Error.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import Office from "./Office";
 
 function Error() {
     return (
         <>
-        <div className="w-50 h-50 p-5">
+        {/* <div className="w-50 h-50 p-5">
             <h3>Work in Progress</h3>
             <Link to='/' className="btn btn-info mt-3">Back to Home</Link>
-        </div>
+        </div> */}
+        <Office/>
         </>
     )
 }
--- src/component/Office.jsx
+++ src/component/Office.jsx
@@ -2,30 +2,59 @@ import { Typography } from "@mui/material";
 import NavBar from "./NavBar";
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import logo from "../assets/p1.jpeg";
 
 function Office() {
+  const products = [
+    { id: 1, name: "", image: logo },
+    { id: 2, name: "", image: logo },
+    { id: 3, name: "", image: logo },
+    { id: 4, name: "", image: logo },
+    { id: 5, name: "", image: logo },
+    { id: 6, name: "", image: logo },
+    { id: 7, name: "", image: logo },
+    { id: 8, name: "", image: logo },
+    { id: 9, name: "", image: logo },
+    { id: 10, name: "", image: logo },
+  ];
+
   return (
     <>
       <NavBar />
-      <div className="w-100 vh-100" style={{marginTop: "8rem"}}>
+      <div className="w-100 h-100" style={{ marginTop:"8rem", display: "flex",justifyContent:"center",alignSelf:"center"}}>
         <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          '& > :not(style)': {
-            m: 1,
-            width: 128,
-            height: 128,
-          },
-        }}
-      >
-      <Paper elevation={8} />
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />   
-      <Paper elevation={3} />
-      </Box >
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            width:"80%",
+            gap: 2,
+            justifyContent: "center",
+            
+          }}
+        >
+          {products.map((product) => (
+            <Paper
+              key={product.id}
+              sx={{
+                backgroundImage: `url(${product.image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                width: 200,
+                height: 200,
+                display: "flex",
+                alignItems: "flex-end",
+                justifyContent: "center",
+                color: "white",
+                fontWeight: "bold",
+                p: 1,
+                borderRadius: 2,
+                boxShadow: 3,
+              }}
+            >
+              {product.name}
+            </Paper>
+          ))}
+        </Box>
       </div>
     </>
   );
